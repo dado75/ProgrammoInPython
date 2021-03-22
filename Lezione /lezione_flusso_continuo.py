f57c6a24170da38224f65aa0a765be0cecd42408
dado75 <fenealvi.piccolo@gmail.com>
2021-03-22 23:06:18 +0100

    cancello codice in più

--- Lezione /lezione_flusso_continuo.py
+++ Lezione /lezione_flusso_continuo.py
@@ -1,15 +1,9 @@
-#contatore = 0
-#while contatore < 10000:
-#    contatore += 100
-#    if contatore == 5000 :
-#        print("Cosa succede adesso??")
-#       # break
-#    print(contatore)
-
 contatore = 0
-div = 2
-count = 0
-while contatore < 100:
-    contatore += 1
-    if div <= contatore/2 and count==0:
-        print(contatore)
+while contatore < 10000:
+    contatore += 100
+    if contatore == 5000 :
+        print("Cosa succede adesso??")
+       # break
+    print(contatore)
+
+
